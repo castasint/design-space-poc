e253c54ba3b40746215e02b7d7712d65477b6858
Hanuma Pedaprolu <hanuma.pedaprolu@dhl.com>
2024-04-15 17:10:21 +0530

    Added column layout

--- components/skeleton.js
+++ components/skeleton.js
@@ -1,31 +1,50 @@
-import * as React from 'react';
+import * as React from "react";
 
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Skeleton from '@mui/material/Skeleton';
-import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Skeleton from "@mui/material/Skeleton";
+import Typography from "@mui/material/Typography";
 
-export default function SkeletonComponent({ title, subTitle, numberOfRows, numberOfColumns }) {
+export default function SkeletonComponent({
+    title,
+    subTitle,
+    numberOfRows,
+    numberOfColumnsPerRow,
+}) {
     return (
         <Box sx={{ marginTop: 10, borderRadius: 5, margin: 5 }}>
-            <Typography variant="h5" component="div" sx={{ letterSpacing: '0.1em', textTransform: 'uppercase', color: 'blue' }}>
+            <Typography
+                variant="h5"
+                component="div"
+                sx={{
+                    letterSpacing: "0.1em",
+                    textTransform: "uppercase",
+                    color: "blue",
+                }}
+            >
                 {title || <Skeleton />}
             </Typography>
-            <Typography variant="subtitle1" component="div" sx={{ color: 'gray', paddingLeft: 2, paddingBottom: 1 }}>
+            <Typography
+                variant="subtitle1"
+                component="div"
+                sx={{ color: "gray", paddingLeft: 2, paddingBottom: 1 }}
+            >
                 {subTitle || <Skeleton />}
             </Typography>
-            {
-                Array.from({ length: numberOfRows }).map((_, rowIndex) => (
-                    <Grid container key={rowIndex} sx={{ marginY: 5 }}>
-
-
-                        <Skeleton animation="wave" variant="rectangular" width="100%" height={75} />
-
-
-
-                    </Grid>
-                ))
-            }
+            {Array.from({ length: numberOfRows }).map((_, rowIndex) => (
+                <Grid container key={rowIndex} sx={{ marginY: 5 }} spacing={2}>
+                    {Array.from({ length: numberOfColumnsPerRow }).map((_, colIndex) => (
+                        <Grid item key={colIndex} xs={12 / numberOfColumnsPerRow}>
+                            <Skeleton
+                                animation="wave"
+                                variant="rectangular"
+                                width="100%"
+                                height={75}
+                            />
+                        </Grid>
+                    ))}
+                </Grid>
+            ))}
         </Box>
     );
 }
